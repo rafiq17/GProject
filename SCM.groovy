b1c1091faa3a65ba0cc2fe5d69e6415758caaa86
Mehaboob <mohamed.r.mehaboob@accenture.com>
2017-08-28 17:17:20 +0530

    Updated

---
+++ SCM.groovy
@@ -0,0 +1,11 @@
+job('example-job-from-job-dsl') {
+    scm {
+        github('https://github.com/rafiq17/GProject.git', 'master')
+    }
+    triggers {
+        cron("@hourly")
+    }
+    steps {
+        shell("echo 'Hello World'")
+    }
+}
